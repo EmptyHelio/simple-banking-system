74cd02bd1db5453251211027dbcc01c0b01a8849
Nikolai Shalaev <wisp-light@yandex.ru>
2021-07-12 22:37:11 +0300

    update method greeting

--- banking-system.py
+++ banking-system.py
@@ -6,6 +6,10 @@ class Bank:
         choice = enquiries.choose('Welcome to the banking system! Choose one of these options:', options)
         if choice == 'Create an account':
             print(colorama.Fore.GREEN + "Account has been created!")
+        elif choice == 'Log into account':
+            print(colorama.Fore.GREEN + "You are successfully logged in")
+        else:
+            print(colorama.Fore.RED + "Good Luck! Bye")
 
 if __name__ == "__main__":
     start_sys = Bank()
