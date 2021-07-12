19f8b43b30b2db55c3088cf0447fb26890878607
Nikolai Shalaev <wisp-light@yandex.ru>
2021-07-12 22:16:52 +0300

    Update .gitignore. Create class Bank

--- banking-system.py
+++ banking-system.py
@@ -1,7 +1,12 @@
 import enquiries
-
-def main():
-    print("helo")
+import colorama
+class Bank:
+    def greeting(self):
+        options = ['Create an account', 'Log into account', 'Exit']
+        choice = enquiries.choose('Welcome to the banking system! Choose one of these options:', options)
+        if choice == 'Create an account':
+            print(colorama.Fore.GREEN + "Account has been created!")
 
 if __name__ == "__main__":
-    main()
+    start_sys = Bank()
+    start_sys.greeting()
